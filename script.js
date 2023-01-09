acba3dd6312a7ddb6966afce6145b7ee6ffe44f6
Jacob Goreham <jacobgoreham@gmail.com>
2023-01-09 12:37:54 -0800

    Added event listener to generate button, Added Functions for writePassword and generatePassword.

--- script.js
+++ script.js
@@ -58,3 +58,31 @@ var upperCaseArr = [
 ];
 var numbersArr = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 var symbolsArr = ["!", "@", "#", "$", "%", "^", "&", "*", "_", "-", "+", "="];
+
+// Assigntment Code
+var generateBtn = document.querySelector("#generate");
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+
+//Write password for the #password input
+function writePassword() {
+  var correctPrompts = getPrompts(); //true or false
+  var passwordText = document.querySelector("#password");
+
+  if (correctPrompts) {
+    var newPassword = generatePassword();
+    passwordText.value = newPassword;
+  } else {
+    passwordText.value = "";
+  }
+}
+
+function generatePassword() {
+  var password = "";
+  for (var i = 0; i > characterLength; i++) {
+    var randomIndex = Math.floor(Math.random() * choiceArr.length);
+    password = password + choiceArr[randomIndex];
+  }
+  return password;
+}
